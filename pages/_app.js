3f2bf386d894bca3134f21b33a5fa39e3b0f4e40
Gherase12 <c_gherase12@yahoo.ro>
2023-01-09 22:43:26 +0200

    as

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,7 @@
 import { useRouter } from "next/router";
 import "../styles/globals.css";
-import Image  from 'next/image';
+import Image from "next/image";
 
 export default function App({ Component, pageProps }) {
-  const router = useRouter();
-  const currentPagePath = router.pathname;
-  if (currentPagePath != "/") {
-    return <Component {...pageProps} />;
-  } else {
-    return <div  className="w-full h-screen   ">
-       <Image src="/coming-soon.webp" fill className="object-contain" alt="coming soon" />
-    </div>;
-  }
+  return <Component {...pageProps} />;
 }
