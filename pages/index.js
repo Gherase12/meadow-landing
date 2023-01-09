13329d1d3ead2a03c35a7c5fbc4cbef2f1332c77
Gherase12 <c_gherase12@yahoo.ro>
2023-01-09 12:33:40 +0200

    add webp imgs

--- pages/index.js
+++ pages/index.js
@@ -10,48 +10,48 @@ import { Pagination } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
 import Nav from "../components/Nav";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 import Link from "next/link";
-import { useRef} from 'react'
+import { useRef } from "react";
 
 export default function Home() {
   // imgs
   const imgs = ["/meadow-card.svg", "/project1.png", "/project2.png"];
   const actionCards = ["/action-card-1.png", "/action-card-2.png"];
   // refs
-  const homeRef = useRef(null)
-  const projectRef = useRef(null)
-  const teamRef = useRef(null)
+  const homeRef = useRef(null);
+  const projectRef = useRef(null);
+  const teamRef = useRef(null);
   // imfo
   const info = [
     {
       name: "3M tokens",
       details: "Initial circulating supply",
-      img: "/object-4.svg",
+      img: "/object-4.webp",
     },
     {
       name: "$0,10",
       details: "Private Price",
-      img: "/object-5.svg",
+      img: "/object-5.webp",
     },
     {
       name: "$600,000",
       details: "Initial Marketcap",
-      img: "/object-6.svg",
+      img: "/object-6.webp",
     },
   ];
   // framer motion variants
   const orizontalVariants = {
-    visible: { opacity: 1, x:0 },
-    hidden: { opacity: 0 , x:100},
-    transition:{duration:0.6}
-  }
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: 100 },
+    transition: { duration: 0.6 },
+  };
   const inViewVariants = {
-    visible: { opacity: 1, y:0 },
-    hidden: { opacity: 0 , y:200},
-    transition:{delay: 4, duration:2}
-  }
+    visible: { opacity: 1, y: 0 },
+    hidden: { opacity: 0, y: 200 },
+    transition: { delay: 4, duration: 2 },
+  };
 
   return (
     <div className='lg:p-[20px] overflow-hidden relative'>
@@ -63,76 +63,81 @@ export default function Home() {
       </Head>
       <Nav homeRef={homeRef} projectRef={projectRef} teamRef={teamRef} />
       {/* header */}
-      <div ref={homeRef}  className='mb-[150px] lg:rounded-t-[40px] lg:rounded-b-[70px]  relative background-gradient   w-full h-[600px] lg:h-[800px] lg:p-[40px] z-10 pt-[30px]  '>
+      <div
+        ref={homeRef}
+        className='mb-[150px] lg:rounded-t-[40px] lg:rounded-b-[70px]  relative background-gradient   w-full h-[600px] lg:h-[800px] lg:p-[40px] z-10 pt-[30px]  '
+      >
         {/* images */}
 
         <div className='absolute hidden lg:flex w-[822px] h-[822px] right-[280px] top-[270px] scale-[1.4] z-30 '>
-          <Image src={"/object-1.svg"} fill alt='object-1' />
+          <Image src={"/object-1.webp"} fill alt='object-1' />
         </div>
 
         <div className='absolute w-[261px] hidden lg:flex scale-[1.6] h-[261px] right-[365px] top-[101px]  z-30 '>
-          <Image src={"/object-2.svg"} fill alt='object-2' />
+          <Image src={"/object-2.webp"} fill alt='object-2' />
         </div>
-        <div className='absolute w-[432px] hidden lg:flex h-[432px] right-0 top-[142px]  z-30 '>
-          <Image src={"/object-3.svg"} fill alt='object-3'  />
+        <div className='absolute   w-[432px] hidden lg:flex h-[432px] -right-[100px] top-[142px]  z-30 '>
+          <Image
+            src={"/object-3.webp"}
+            fill
+            alt='object-3'
+            className='object-contain'
+          />
         </div>
         {/* nav */}
 
-    
-
         {/* h1 */}
-        <motion.h1 
-        
-        initial="hidden"
-        animate="visible"
-        variants={orizontalVariants}
-        className='absolute  z-40 text-white leading-[110%] top-[90px]  lg:top-[154px] left-[30px] lg:left-[152px] w-[80vw]  lg:w-[785px] lg:h-[243px] text-[30px] md:text-[50px] lg:text-[74px] font-bold '>
+        <motion.h1
+          initial='hidden'
+          animate='visible'
+          variants={orizontalVariants}
+          className='absolute  z-40 text-white leading-[110%] top-[90px]  lg:top-[154px] left-[30px] lg:left-[152px] w-[80vw]  lg:w-[785px] lg:h-[243px] text-[30px] md:text-[50px] lg:text-[74px] font-bold '
+        >
           The Next Generation Web 3.0 Multichain Launchpad
         </motion.h1>
         {/* h2 */}
-        <motion.h2 
-        initial="hidden"
-        animate="visible"
-        variants={orizontalVariants}
-        className='absolute text-white top-[275px]  h-[210px] text-ellipsis overflow-hidden lg:top-[432px] lg:left-[152px] text-[12px] lg:text-[16px] leading-[160%] w-[70vw]  lg:w-[702px] font-normal left-[30px] z-40'>
-          Meadow is a decentralized launchpad that will launch projects on the
-          Sui Network. The Sui Network will be one of the most unique Layer 1
-          networks. Meadow has been in the works for a number of months, and it
-          is at this point poised to cause a ruckus in the industry. The
-          experience that the Meadow teams bring to the table includes working
-          with various prominent incubators as well as top-tier businesses.
-          Before launching, all projects on Meadow will go through a rigorous
-          process of due diligence to ensure their legitimacy.
+        <motion.h2
+          initial='hidden'
+          animate='visible'
+          variants={orizontalVariants}
+          className='absolute text-white top-[275px]  h-[210px] text-ellipsis overflow-hidden lg:top-[432px] lg:left-[152px] text-[12px] lg:text-[16px] leading-[160%] w-[70vw]  lg:w-[702px] font-normal left-[30px] z-40'
+        >
+          Built for the next generation of users, Meadow will Incubate and
+          launch the most anticipated projects on the Sui Network. Meadow's team
+          consists of veterans and partners who bring years of qualified
+          experience in the incubation space which puts us far ahead of our
+          competition.
         </motion.h2>
         {/* buttons */}
         <div className='flex absolute bottom-[30px] scale-[0.6] lg:scale-[1]  lg:bottom-[100px] -left-[30px] lg:left-[152px] space-x-[20px] z-40'>
-          <Link href="https://app.meadowlaunch.com/"> 
-          <button  className='bg-white text-[18px]  shadow-lg text-black w-[187px] h-[56px] rounded-full '>
-            Dashboard{" "}
-          </button>
+          <Link href='https://app.meadowlaunch.com/'>
+            <button className='bg-white text-[18px]  shadow-lg text-black w-[187px] h-[56px] rounded-full '>
+              Dashboard{" "}
+            </button>
           </Link>
           <div className='text-white text-[40px] space-x-[10px] flex items-center'>
-            <Link href="https://twitter.com/meadowlaunch">
-            <BsTwitter />
+            <Link href='https://twitter.com/meadowlaunch'>
+              <BsTwitter />
             </Link>
-            <Link href="https://t.co/FLNKZU3ujp" >
-            <FaDiscord />
+            <Link href='https://t.co/FLNKZU3ujp'>
+              <FaDiscord />
             </Link>
-            <Link href="https://www.instagram.com/meadow_launch/?hl=ro">            
-            <AiOutlineInstagram />
+            <Link href='https://www.instagram.com/meadow_launch/?hl=ro'>
+              <AiOutlineInstagram />
             </Link>
-            
           </div>
         </div>
       </div>
       {/* IDO's */}
 
       <motion.div
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      variants={inViewVariants}
-      ref={projectRef} className=' mb-[50px] lg:mb-[198px] '>
+        initial='hidden'
+        whileInView='visible'
+        viewport={{ once: true }}
+        variants={inViewVariants}
+        ref={projectRef}
+        className=' mb-[50px] lg:mb-[198px] '
+      >
         <h3 className='leading-[110%] lg:ml-0 text-[48px] text-black ml-[20px]  lg:pl-[180px] mb-0 lg:mb-[52px] '>
           Upcoming IDOs
         </h3>
@@ -159,23 +164,27 @@ export default function Home() {
       </Swiper>
 
       {/* Upcoming Game IDOs */}
-      <motion.div 
-       initial="hidden"
-       whileInView="visible"
-       viewport={{ once: true }}
-       variants={inViewVariants}
-      className='mb-[50px] lg:mb-[198px]'>
+      <motion.div
+        initial='hidden'
+        whileInView='visible'
+        viewport={{ once: true }}
+        variants={inViewVariants}
+        className='mb-[50px] lg:mb-[198px]'
+      >
         <h3 className='leading-[110%] text-[48px] lg:ml-0 text-black lg:pl-[180px] mb-[52px] ml-[20px]'>
           Upcoming Game IDOs
         </h3>
         <div className=' flex flex-col lg:flex-row  justify-center lg:space-x-[60px]  '>
           {[1, 2].map((n, i) => (
-            <div className='relative w-full h-[250px] lg:w-[650px] lg:h-[406px]   ' key={i} >
+            <div
+              className='relative w-full h-[250px] lg:w-[650px] lg:h-[406px]   '
+              key={i}
+            >
               <Image
                 src={`/game-card-${i + 1}.svg`}
                 fill
                 calssName='object-cover'
-                alt="game-card"
+                alt='game-card'
               />
             </div>
           ))}
@@ -183,11 +192,12 @@ export default function Home() {
       </motion.div>
       {/* privete round */}
       <motion.div
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      variants={inViewVariants}
-      className='flex   justify-center lg:space-x-[76px] mb-[50px] lg:mb-[152px] flex-col lg:flex-row space-y-[100px] lg:space-y-0'>
+        initial='hidden'
+        whileInView='visible'
+        viewport={{ once: true }}
+        variants={inViewVariants}
+        className='flex   justify-center lg:space-x-[76px] mb-[50px] lg:mb-[152px] flex-col lg:flex-row space-y-[100px] lg:space-y-0'
+      >
         <div className=''>
           <h3 className='leading-[110%] lg:text-[48px] text-black mb-[30px]  text-[30px] lg:ml-0 ml-[20px] w-[300px] lg:w-[426px] h-[106px]'>
             Private Round & Token Information
@@ -200,8 +210,7 @@ export default function Home() {
             information.
           </p>
         </div>
-        
-        
+
         <div className='  flex flex-col space-y-[60px] '>
           {info.map(({ name, details, img }, i) => (
             <Info name={name} details={details} img={img} key={i} />
@@ -209,42 +218,49 @@ export default function Home() {
         </div>
       </motion.div>
 
-
       {/* action cards */}
       <motion.div
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      variants={inViewVariants}
-      className='flex    lg:justify-center lg:overflow-hidden flex-row space-x-[10px] lg:space-x-[10px] pl-[20px]  z-10 mb-[60px] lg:mb-[152px] overflow-x-scroll '>
-
-        {actionCards.map((c,i) => (
-          <div className=' relative flex-shrink-0  w-[300px] h-[200px] lg:w-[650px] lg:h-[463px]  ' key={i}>
-            <Image src={c} fill className='object-cover rounded-[20px]  lg:rounded-[55px] ' alt="action-card" />
+        initial='hidden'
+        whileInView='visible'
+        viewport={{ once: true }}
+        variants={inViewVariants}
+        className='flex    lg:justify-center lg:overflow-hidden flex-row space-x-[10px] lg:space-x-[10px] pl-[20px]  z-10 mb-[60px] lg:mb-[152px] overflow-x-scroll '
+      >
+        {actionCards.map((c, i) => (
+          <div
+            className=' relative flex-shrink-0  w-[300px] h-[200px] lg:w-[650px] lg:h-[463px]  '
+            key={i}
+          >
+            <Image
+              src={c}
+              fill
+              className='object-cover rounded-[20px]  lg:rounded-[55px] '
+              alt='action-card'
+            />
           </div>
         ))}
-
       </motion.div>
-      
-
 
       {/* team */}
 
       <motion.h3
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      variants={orizontalVariants}
-      ref={teamRef} className='leading-[110%] lg:ml-0 mx-auto text-[48px] text-black  lg:text-[60px] mb-[52px] w-full lg:w-auto  text-center '>
+        initial='hidden'
+        whileInView='visible'
+        viewport={{ once: true }}
+        variants={orizontalVariants}
+        ref={teamRef}
+        className='leading-[110%] lg:ml-0 mx-auto text-[48px] text-black  lg:text-[60px] mb-[52px] w-full lg:w-auto  text-center '
+      >
         Our Team
       </motion.h3>
       {/* slider */}
       <motion.div
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      variants={inViewVariants}
-      className=' max-w-[1500px] mx-auto mb-[100px] '>
+        initial='hidden'
+        whileInView='visible'
+        viewport={{ once: true }}
+        variants={inViewVariants}
+        className=' max-w-[1500px] mx-auto mb-[100px] '
+      >
         <Swiper
           slidesPerView={"auto"}
           spaceBetween={30}
@@ -261,7 +277,7 @@ export default function Home() {
                   src={`/team/member-${i + 1}.svg`}
                   fill
                   className='object-contain shadow-lg rounded-[30px]'
-                  alt="team member"
+                  alt='team member'
                 />
               </div>
             </SwiperSlide>
@@ -270,29 +286,32 @@ export default function Home() {
       </motion.div>
       {/* join section */}
       <motion.div
-      initial="hidden"
-      whileInView="visible"
-      viewport={{ once: true }}
-      variants={inViewVariants}
-      className='h-[440px]  max-w-[1500px] mx-auto relative flex justify-center items-center  flex-col space-y-[30px]'>
+        initial='hidden'
+        whileInView='visible'
+        viewport={{ once: true }}
+        variants={inViewVariants}
+        className='h-[440px]  max-w-[1500px] mx-auto relative flex justify-center items-center  flex-col space-y-[30px]'
+      >
         <Image
-          src='/join-background.svg'
+          src='/join-background.webp'
           className='object-cover lg:rounded-t-[40px] z-10 '
           fill
-          alt="join-card"
+          alt='join-card'
         />
-        <h2 className='text-white lg:text-[60px] text-[40px] z-40 '>Join right now</h2>
+        <h2 className='text-white lg:text-[60px] text-[40px] z-40 '>
+          Join right now
+        </h2>
         <p className='px-[10px] text-[16px] leading-[160%] text-white z-40 lg:w-[462px] lg:h-[78px] text-center  '>
           Meadow is ready to launch the most fascinating tier 1 projects on the
           most scalable, safest Layer 1blockchain, with a team of highly
           qualified experts who have trackrecord of success.
         </p>
         <div className='flex z-40 space-x-[20px] scale-[0.7] '>
-        <Link href="https://app.meadowlaunch.com/"> 
-          <button className='bg-white text-black w-[187px] h-[56px] rounded-full z-40 '>
-            Dashboard
-          </button>
-        </Link>
+          <Link href='https://app.meadowlaunch.com/'>
+            <button className='bg-white text-black w-[187px] h-[56px] rounded-full z-40 '>
+              Dashboard
+            </button>
+          </Link>
           <button className=' border-white   text-white w-[187px] h-[56px] rounded-full z-40 '>
             Apply now
           </button>
@@ -302,25 +321,24 @@ export default function Home() {
       <div className='max-w-[1500px] mx-auto flex justify-between items-center mt-[30px] '>
         <div className='text-black  space-x-[20px] text-[20px] hidden lg:flex'>
           <p className='text-[14px]   '>Follow us on</p>
-          <Link href="https://twitter.com/meadowlaunch">
+          <Link href='https://twitter.com/meadowlaunch'>
             <BsTwitter />
-            </Link>
-            <Link href="https://t.co/FLNKZU3ujp" >
+          </Link>
+          <Link href='https://t.co/FLNKZU3ujp'>
             <FaDiscord />
-            </Link>
-            <Link href="https://www.instagram.com/meadow_launch/?hl=ro">            
+          </Link>
+          <Link href='https://www.instagram.com/meadow_launch/?hl=ro'>
             <AiOutlineInstagram />
-            </Link>
+          </Link>
         </div>
-        <Link href="/">
-        
-        <Image
-          src='/meadow-logo-black.svg'
-          className='ml-[50px] hidden xl:flex'
-          width={186}
-          height={58}
-          alt="meadow-logo"
-        />
+        <Link href='/'>
+          <Image
+            src='/meadow-logo-black.svg'
+            className='ml-[50px] hidden xl:flex'
+            width={186}
+            height={58}
+            alt='meadow-logo'
+          />
         </Link>
         <li className='text-[10px] px-[10px] mb-[10px] lg:mb-0 flex justify-between space-x-[10px] lg:text-[14px] cursor-pointe  w-full xl:w-auto '>
           <ul>Privacy Policy</ul>
